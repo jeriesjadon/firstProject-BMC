9383e654a7dc8b9e6dceaa89c0886e097319f102
jeriesjadon <49919328+jeriesjadon@users.noreply.github.com>
2022-12-27 12:07:16 +0200

    Update myproject.py

--- myproject.py
+++ myproject.py
@@ -1,1 +1,28 @@
-print("hello world)
+import requests
+import bs4
+
+def MyScrap(site):
+
+ try:
+    res= requests.get(site)
+ except Exception as e:
+    print("ERROR, >>",e)
+    exit()
+ soup = bs4.BeautifulSoup(res.text, 'html.parser')
+ for link in soup.find_all('a', href=True):
+     print(link['href'])
+
+
+
+def main():
+    full_path='http://example.webscraping.com'
+    #site = input("please enter the the HOSTNAME you want to scarp :")
+    #path = input("please enter from where you want to start the scarping ")
+    #full_path = site + path
+    MyScrap(full_path)
+
+
+
+if __name__ == "__main__":
+
+    main()
